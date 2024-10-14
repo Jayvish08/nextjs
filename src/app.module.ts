c36dad86b64128e9e2eaf82f435e7ca0e2047370
Jay Vishwakarma <jayvish08@gmail.com>
2024-10-14 17:19:18 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './controllers/user.controller';
+import { User } from './entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type : 'postgres',
+      host : 'localhost',
+      port : 5432,
+      username : 'postgres',
+      password : 'jayvish08',
+      database : 'taskmanagement',
+      entities : [User],
+      synchronize : true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [UserController],
+})
+export class AppModule {}
